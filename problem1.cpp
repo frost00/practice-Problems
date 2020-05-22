73b9e8b80121fd1476afc728bc3957bf0ab19187
Shayon <34762596+frost00@users.noreply.github.com>
2020-05-22 00:06:40 -0400

    problem one

---
+++ problem1.cpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   problem1.cpp
+ * Author: shayonmaramkhah
+ *
+ * Created on May 21, 2020, 11:29 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+
+
+
+int main() {
+    
+    std::cout<<"Enter first number"<<std::endl;
+    unsigned valueOne,valueTwo;
+    std::cin >> valueOne;
+    std::cin >> valueTwo;
+    printf("sum = %d ",valueOne+valueTwo);
+    
+    return 0;
+}
