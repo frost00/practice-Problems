b2a72fca6f10ab683241d542c0fb72a8076ea022
Shayon <34762596+frost00@users.noreply.github.com>
2020-05-29 00:43:11 -0400

    problem8

---
+++ problem8.cpp
@@ -0,0 +1,33 @@
+/*
+ * SIGCPP
+ * Programmer: Shayon Maramkhah
+ * 
+    Description: Swap  (that  is,  interchange)  the  content  of  two  floating-point  variables a
+ * and b, both  of which  are initialized by the user. Print the values in both 
+ * variables before swapping and after swapping.
+ * 
+ */
+
+#include <iostream>
+#include <math.h>
+
+int main() {
+    
+    float a,b,c;
+    std::cout<<"Enter number a and b:"<<std::endl;
+    std::cin>>a;
+    std::cin>>b;
+    
+    printf("User input a: %.2f and User input b: %.2f ",a,b);
+    
+    c= a+b;
+    a=c-a;
+    b=c-b;
+    printf("Swapped input a: %.2f and User input b: %.2f \n",a,b);
+
+    
+    
+    return 0;
+}
+
+
